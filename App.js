46a57b4391835dec7365196c247bda96a6297e28
LauraMolano <laurapaola.molano172@comunidadunir.net>
2025-12-09 07:26:15 -0500

    nstalación correcta de fuentes y corriendo

--- App.js
+++ App.js
@@ -1,22 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ActivityIndicator } from 'react-native';
 import "./global.css";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'DMSansRegular': require('./assets/fonts/DM_Sans/DMSansRegular.ttf'),
+    'DMSansBold': require('./assets/fonts/DM_Sans/DMSansBold.ttf'),
+    'DMSansItalic': require('./assets/fonts/DM_Sans/DMSansItalic.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+    <View className="flex-1 bg-blue-500 items-center justify-center">
+      <Text 
+        className="text-4xl text-white font-bold mb-4" 
+        style={{ fontFamily: 'DMSansBold' }}
+      >Prueba
       </Text>
+      <StatusBar style="light" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
